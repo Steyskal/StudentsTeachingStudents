5d2d0bf0237fa8b228b815647a18385124f24ec4
Marko Alerić <maleric@foi.hr>
2015-09-29 16:05:09 +0200

    Samo skripta za kameru dodana

---
+++ Enemies/SPS - Enemies/Assets/Scripts/SimpleCameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleCameraMovement : MonoBehaviour {
+	public Transform target;
+	
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = target.position + offset;
+		//transform.position = Vector3.Lerp(transform.position, target.transform.position + offset,0.11f);
+
+	}
+}
